d4798efe2791c7052a11764476a3736186dcb230
sendcraig <cmsendme@gmail.com>
2023-11-11 16:38:57 +0100

    set up Sass, remove starter UI code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <h1 style={{ fontSize: 72 }}>🐘❤️🦒</h1>
+      <h3>Coming September 2024</h3>
     </div>
   );
 }
