dc0f10ee2f1dc83ad802f88b1b623882cf424bbe
sendcraig <cmsendme@gmail.com>
2023-11-14 22:51:23 +0100

    spacing & scaling fixes for text

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,20 @@ function App() {
           <h1 className={cx(styles.heroText)}>DATE</h1>
         </div>
         <div className={styles.mainTextContainer}>
-          <h2 className={styles.nameText}>Craig & Josephin</h2>
-          <h3 className={styles.infoTextBold}>September 29th, 2024</h3>
-          <h3 className={styles.infoTextBold}>Cambos Estate, Crete, Greece</h3>
-          <h3 className={styles.infoText}>More information to follow</h3>
+          <div className={styles.nameTextContainer}>
+            <h2 className={styles.nameText}>Craig</h2>
+            <h3 className={styles.ampersand}>&</h3>
+            <h2 className={styles.nameText}>Josephin</h2>
+          </div>
+          <div>
+            <h3 className={styles.infoTextBold}>September 29th, 2024</h3>
+            <h3 className={styles.infoTextBold}>
+              Cambos Estate, Crete, Greece
+            </h3>
+          </div>
+          <h3 className={styles.infoText}>
+            Official invitation & details to follow
+          </h3>
         </div>
       </div>
     </div>
