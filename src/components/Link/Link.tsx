f0e48b0992a0525283c3a15742f220f6fbbe2858
sendcraig <cmsendme@gmail.com>
2023-11-19 17:06:08 +0100

    update ical link

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,11 +1,16 @@
 import React, { JSX } from 'react';
 import styles from './Link.module.scss';
 
-type Props = { url: string; text: string };
+type Props = { url: string; text: string; newTab?: boolean };
 
-export const Link = ({ url, text }: Props): JSX.Element => {
+export const Link = ({ url, text, newTab = true }: Props): JSX.Element => {
   return (
-    <a target={'_blank'} rel="noreferrer" href={url} className={styles.link}>
+    <a
+      target={newTab ? '_blank' : undefined}
+      rel="noreferrer"
+      href={url}
+      className={styles.link}
+    >
       {text}
     </a>
   );
