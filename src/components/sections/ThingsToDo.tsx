0223eef29040d418c365ec7ad485935eb27519f4
sendcraig <cmsendme@gmail.com>
2023-12-08 03:30:04 +0100

    add content & information

--- src/components/sections/ThingsToDo.tsx
+++ src/components/sections/ThingsToDo.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { Center, Grid, Stack } from '@mantine/core';
+import { Center, Grid, Stack, TypographyStylesProvider } from '@mantine/core';
 import { InfoSection } from '../InfoSection/InfoSection';
 import { Body, SectionTitle, SubHeading } from '../typography/Typography';
 import styles from './ThingsToDo.module.scss';
@@ -8,23 +8,19 @@ import { useMediaQuery } from '@mantine/hooks';
 const THINGS_TO_DO_SECTIONS = [
   {
     title: 'Beaches & Nature',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget scelerisque felis, quis venenatis dolor. Cras laoreet nulla eget est viverra, non posuere leo accumsan. Nunc maximus aliquam elit malesuada iaculis. Etiam vel nisl pretium, imperdiet nibh ac, tempus nisi. Donec sodales vel massa quis fermentum. ',
+    body: 'Crete is famous for its nature, and for its beaches in particular. The west side of the island is home to many fantastic beaches, the best and most famous of which are Balos and Elafonissi, but Falasarna, Marathi, and Agia Marina beaches are also popular.<br/><br/>For the more adventurous, there is hiking in the southern and more mountainous parts of the island. The Kourtaliotiko Gorge in particular is known to be one of the most beautiful natural areas of the entire island, and is a little more than an hour drive from Chania.',
   },
   {
     title: 'City & Culture',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget scelerisque felis, quis venenatis dolor. Cras laoreet nulla eget est viverra, non posuere leo accumsan. Nunc maximus aliquam elit malesuada iaculis. Etiam vel nisl pretium, imperdiet nibh ac, tempus nisi. Donec sodales vel massa quis fermentum. ',
+    body: 'Chania is the biggest city in Crete, and has a beautiful Old Town and historic center. The large harbor in the city center is surrounded with restaurants, cafés, and shops as well as many historic sights. There are walking tours for those interested in learning more about the city.<br/><br/>The towns and villages near Chania are also full of charm, such as Kalyves, Almyrida, Vamos, and Gavalochori to the east, and Kissamos, Elos, and Kaliviani to the west.',
   },
   {
     title: 'Eat & Drink',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget scelerisque felis, quis venenatis dolor. Cras laoreet nulla eget est viverra, non posuere leo accumsan. Nunc maximus aliquam elit malesuada iaculis. Etiam vel nisl pretium, imperdiet nibh ac, tempus nisi. Donec sodales vel massa quis fermentum. ',
-  },
-  {
-    title: 'Nightlife & Party',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget scelerisque felis, quis venenatis dolor. Cras laoreet nulla eget est viverra, non posuere leo accumsan. Nunc maximus aliquam elit malesuada iaculis. Etiam vel nisl pretium, imperdiet nibh ac, tempus nisi. Donec sodales vel massa quis fermentum. ',
+    body: 'There is an endless amount of delicious food in Crete, with Greek cuisine obviously being the star of the show. There are many good restaurants in the city center near the harbor, but also quite a few hidden gems in the less touristy areas. We also recommend travelling to nearby villages to visit an authentic taverna in the countryside and enjoy some Raki with your meal and the famous Cretan hospitality. We\'ve started compiling a list of some of our favorites near Chania that we\'ve either visited or been recommended, <a href="https://maps.app.goo.gl/nwdFhfzkBoDAfDKa7" target="_blank">you can find it here.</a>',
   },
   {
     title: 'Arts & History',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget scelerisque felis, quis venenatis dolor. Cras laoreet nulla eget est viverra, non posuere leo accumsan. Nunc maximus aliquam elit malesuada iaculis. Etiam vel nisl pretium, imperdiet nibh ac, tempus nisi. Donec sodales vel massa quis fermentum. ',
+    body: "Greece in general is rich with history, and Crete is no exception. There are numerous ruins and landmarks, mostly concentrated in the eastern part of the island. There are numerous museums in Chania where one can learn more about the ancient and more modern history of the area.<br/><br/>On the history topic, if you're stopping in Athens on the way to Crete visiting the Acropolis is a must. The iconic structure itself is impressive, but its hilltop location also offers 360° panoramic views of the city.",
   },
 ];
 
@@ -33,9 +29,11 @@ const Section = ({ title, body }: { title: string; body: string }) => {
     <Center>
       <Stack>
         <SubHeading center>{title}</SubHeading>
-        <Body center className={styles.section}>
-          {body}
-        </Body>
+        <TypographyStylesProvider p={0}>
+          <Body center className={styles.section}>
+            <div dangerouslySetInnerHTML={{ __html: body }} />
+          </Body>
+        </TypographyStylesProvider>
       </Stack>
     </Center>
   );
--- src/components/sections/Travel.tsx
+++ src/components/sections/Travel.tsx
@@ -1,20 +1,20 @@
 import React, { ReactElement } from 'react';
-import { Center, Stack } from '@mantine/core';
+import { Center, Stack, TypographyStylesProvider } from '@mantine/core';
 import { InfoSection } from '../InfoSection/InfoSection';
 import { Body, SectionTitle, SubHeading } from '../typography/Typography';
 
 const TRAVEL_SECTIONS = [
   {
-    title: 'Getting to Chania',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget scelerisque felis, quis venenatis dolor. Cras laoreet nulla eget est viverra, non posuere leo accumsan. Nunc maximus aliquam elit malesuada iaculis. Etiam vel nisl pretium, imperdiet nibh ac, tempus nisi. Donec sodales vel massa quis fermentum. ',
+    title: 'Getting there',
+    body: "The wedding venue is located in Chania, Greece on the island of Crete. Chania has an airport with connections to many major European airports and cities. If you're interested in spending some more time in Greece, it may be worthwhile to search for international flights through Athens and then take a small domestic flight or ferry from there to Crete.",
   },
   {
     title: 'Getting around',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget scelerisque felis, quis venenatis dolor. Cras laoreet nulla eget est viverra, non posuere leo accumsan. Nunc maximus aliquam elit malesuada iaculis. Etiam vel nisl pretium, imperdiet nibh ac, tempus nisi. Donec sodales vel massa quis fermentum. ',
+    body: 'To get around Crete it is advised to rent a car, although there are also taxi services available. In Athens the best way to get around is either with the metro or Uber, but renting a car is impractical.<br/><br/>For the wedding ceremony, there will be shuttles to transport guests from their accommodation to the venue and back. There are many car rental companies with competitive rates in Crete, to save you a Google: <ul xmlns="http://www.w3.org/1999/html"><li>x</li><li>x</li><li>z</li></ul>',
   },
   {
     title: 'Where to stay',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget scelerisque felis, quis venenatis dolor. Cras laoreet nulla eget est viverra, non posuere leo accumsan. Nunc maximus aliquam elit malesuada iaculis. Etiam vel nisl pretium, imperdiet nibh ac, tempus nisi. Donec sodales vel massa quis fermentum. ',
+    body: 'There are many hotels and Airbnbs in Chania city, and many beautiful villas in the surrounding areas for larger parties. We have reserved a block of rooms in the <todo - hotel> hotel if you use the code(?). Some reputable sources for accomodations are: <ul><li>Booking.com</li><li>Airbnb</li><li>Etouri villas</li><li>Other villa site?</li></ul>',
   },
 ];
 
@@ -31,7 +31,12 @@ export const Travel = ({ scrollRef }: Props): ReactElement => {
           {TRAVEL_SECTIONS.map((section, idx) => (
             <Stack key={`travel-section-${idx}`} pb={24}>
               <SubHeading>{section.title}</SubHeading>
-              <Body>{section.body}</Body>
+              {/*<Body>{section.body}</Body>*/}
+              <TypographyStylesProvider p={0}>
+                <Body>
+                  <div dangerouslySetInnerHTML={{ __html: section.body }} />
+                </Body>
+              </TypographyStylesProvider>
             </Stack>
           ))}
         </Stack>
